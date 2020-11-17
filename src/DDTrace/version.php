e0ddeb65ad0448e8912a5e5ad6a94c47f3860085
Owais Lone <owais@lone.pw>
2020-11-18 00:45:57 +0530

    Fixed version release number

--- src/DDTrace/version.php
+++ src/DDTrace/version.php
@@ -2,4 +2,4 @@
 
 // Must begin with a number for Debian packaging requirements
 // Must use single-quotes for packaging script to work
-return '0.30.2-beta-sfx3'; // Update Tracer::VERSION too
+return '0.30.2-beta-sfx4'; // Update Tracer::VERSION too
