e4687736c4490494699a92db3215e71d1055f4f7
Siim Kallas <siimkallas@gmail.com>
2021-05-14 10:42:27 +0300

    Add Drupal to php8, php5 extensions list. Fix deferred loading for php7

--- ext/php7/integrations/integrations.c
+++ ext/php7/integrations/integrations.c
@@ -133,7 +133,7 @@ void ddtrace_integrations_rinit(TSRMLS_D) {
     DD_SET_UP_DEFERRED_LOADING_BY_METHOD(DDTRACE_INTEGRATION_YII, "yii\\di\\Container", "__construct",
                                          "DDTrace\\Integrations\\Yii\\YiiIntegration");
 
-    DD_SET_UP_DEFERRED_LOADING_BY_METHOD(DDTRACE_INTEGRATION_DRUPAL, "Drupal\Core\DrupalKernel", "__construct",
+    DD_SET_UP_DEFERRED_LOADING_BY_METHOD(DDTRACE_INTEGRATION_DRUPAL, "Drupal\\Core\\DrupalKernel", "__construct",
                                          "DDTrace\\Integrations\\Drupal\\DrupalIntegration");
 }
 
