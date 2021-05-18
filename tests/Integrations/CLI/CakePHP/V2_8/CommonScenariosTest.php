cb226bba569575652cfd68add687f43d0de717b0
Siim Kallas <siimkallas@gmail.com>
2021-05-18 18:18:31 +0300

    cakephp: clear dumped data for cli test

--- tests/Integrations/CLI/CakePHP/V2_8/CommonScenariosTest.php
+++ tests/Integrations/CLI/CakePHP/V2_8/CommonScenariosTest.php
@@ -21,7 +21,7 @@ class CommonScenariosTest extends CLITestCase
 
     public function testCommandWithNoArguments()
     {
-        $traces = $this->getParsedTracesFromCommand();
+        $traces = $this->inCli($this->getScriptLocation(), self::getEnvs());
 
         $this->assertSpans($traces, [
             SpanAssertion::build(
@@ -37,7 +37,7 @@ class CommonScenariosTest extends CLITestCase
 
     public function testCommandWithArgument()
     {
-        $traces = $this->getParsedTracesFromCommand('command_list');
+        $traces = $this->inCli($this->getScriptLocation(), self::getEnvs(), [], 'command_list');
 
         $this->assertSpans($traces, [
             SpanAssertion::build(
