2b25eaaf4a1ef62c6156e026d1f4c560d74890ee
t2t2 <taavot@gmail.com>
2021-08-04 13:42:30 +0300

    Add to drupal traced functions
    drupal_cron_run, drupal_http_request, module_invoke, module_invoke_all

--- tests/Integrations/Drupal/V7/CommonScenariosTest.php
+++ tests/Integrations/Drupal/V7/CommonScenariosTest.php
@@ -54,40 +54,80 @@ final class CommonScenariosTest extends WebFrameworkTestCase
                     SpanAssertion::exists('PDO.prepare'),
                     SpanAssertion::exists('PDOStatement.execute'),
                     SpanAssertion::exists('PDOStatement.execute'),
-                    SpanAssertion::exists('_drupal_bootstrap_full')->withChildren(
-                        array_merge(
+                    SpanAssertion::exists('_drupal_bootstrap_full')->withChildren(array_merge(
                             array_fill(0, 5, SpanAssertion::exists('PDO.prepare')),
                             array_fill(0, 5, SpanAssertion::exists('PDOStatement.execute'))
-                        )
-                    ),
+                    )),
                     SpanAssertion::exists('_drupal_bootstrap_page_cache')->withChildren([
                         SpanAssertion::exists('PDO.prepare'),
                         SpanAssertion::exists('PDOStatement.execute'),
                         SpanAssertion::exists('_drupal_bootstrap_database'),
-                        SpanAssertion::exists('_drupal_bootstrap_variables')->withChildren(
-                            array_merge(
-                                [
-                                    SpanAssertion::exists('PDO.__construct'),
-                                    SpanAssertion::exists('PDO.exec'),
-                                    SpanAssertion::exists('PDO.exec'),
-                                ],
-                                array_fill(0, 7, SpanAssertion::exists('PDO.prepare')),
-                                array_fill(0, 7, SpanAssertion::exists('PDOStatement.execute'))
-                            )
-                        ),
+                        SpanAssertion::exists('_drupal_bootstrap_variables')->withChildren(array_merge(
+                            [
+                                SpanAssertion::exists('PDO.__construct'),
+                                SpanAssertion::exists('PDO.exec'),
+                                SpanAssertion::exists('PDO.exec'),
+                            ],
+                            array_fill(0, 7, SpanAssertion::exists('PDO.prepare')),
+                            array_fill(0, 7, SpanAssertion::exists('PDOStatement.execute'))
+                        )),
                     ]),
-                    SpanAssertion::exists('_drupal_bootstrap_page_header'),
+                    SpanAssertion::exists('_drupal_bootstrap_page_header')->withChildren([
+                        SpanAssertion::exists('module_invoke', 'overlay_boot'),
+                        SpanAssertion::exists('module_invoke', 'dblog_boot'),
+                    ]),
                     SpanAssertion::exists('drupal_language_initialize'),
                     SpanAssertion::exists('drupal_session_initialize'),
                     SpanAssertion::exists('menu_execute_active_handler')->withChildren([
                         SpanAssertion::exists('PDO.prepare'),
                         SpanAssertion::exists('PDOStatement.execute'),
-                        SpanAssertion::exists('drupal_deliver_page')->withChildren(
-                            array_merge(
-                                array_fill(0, 72, SpanAssertion::exists('PDO.prepare')),
-                                array_fill(0, 72, SpanAssertion::exists('PDOStatement.execute'))
-                            )
-                        ),
+                        SpanAssertion::exists('drupal_deliver_page')->withChildren(array_merge(
+                            array_fill(0, 26, SpanAssertion::exists('PDO.prepare')),
+                            array_fill(0, 26, SpanAssertion::exists('PDOStatement.execute')),
+                            [
+                                SpanAssertion::exists('drupal_cron_run')->withChildren(array_merge(
+                                    array_fill(0, 5, SpanAssertion::exists('PDO.prepare')),
+                                    array_fill(0, 5, SpanAssertion::exists('PDOStatement.execute')),
+                                    [
+                                        SpanAssertion::exists('module_invoke', 'system_cron')->withChildren(array_merge(
+                                            array_fill(0, 15, SpanAssertion::exists('PDO.prepare')),
+                                            array_fill(0, 15, SpanAssertion::exists('PDOStatement.execute')),
+                                        )),
+                                        SpanAssertion::exists('module_invoke', 'search_cron')->withChildren([
+                                            SpanAssertion::exists('PDO.prepare'),
+                                            SpanAssertion::exists('PDOStatement.execute'),
+                                        ]),
+                                        SpanAssertion::exists('module_invoke', 'node_cron')->withChildren([
+                                            SpanAssertion::exists('PDO.prepare'),
+                                            SpanAssertion::exists('PDOStatement.execute'),
+                                        ]),
+                                        SpanAssertion::exists('module_invoke', 'field_cron')->withChildren(array_merge(
+                                            array_fill(0, 11, SpanAssertion::exists('PDO.prepare')),
+                                            array_fill(0, 11, SpanAssertion::exists('PDOStatement.execute')),
+                                        )),
+                                        SpanAssertion::exists('module_invoke', 'dblog_cron')->withChildren([
+                                            SpanAssertion::exists('PDO.prepare'),
+                                            SpanAssertion::exists('PDOStatement.execute'),
+                                        ]),
+                                        SpanAssertion::exists('module_invoke_all', 'watchdog')->withChildren([
+                                            SpanAssertion::exists('PDO.prepare'),
+                                            SpanAssertion::exists('PDOStatement.execute'),
+                                        ]),
+                                        SpanAssertion::exists('module_invoke_all', 'cron_queue_info'),
+                                    ]
+                                )),
+                                SpanAssertion::exists('module_invoke'),
+                                SpanAssertion::exists('module_invoke'),
+                                SpanAssertion::exists('module_invoke'),
+                                SpanAssertion::exists('module_invoke'),
+                                SpanAssertion::exists('module_invoke'),
+                                SpanAssertion::exists('module_invoke'),
+                                SpanAssertion::exists('module_invoke_all'),
+                                SpanAssertion::exists('module_invoke_all'),
+                                SpanAssertion::exists('module_invoke_all'),
+                            ]
+                        )),
+                        SpanAssertion::exists('module_invoke_all'),
                     ]),
                 ]),
             ]
