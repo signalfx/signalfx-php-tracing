001c5f87b4a9725134e6adc6500c29fe2eeade90
Bob Weinand <bob.weinand@datadoghq.com>
2021-09-02 17:53:05 +0200

    Add functions ZAI support for PHP 5 and 7 (#1300)
    * Add functions ZAI support for PHP 5 and 7
    
    Signed-off-by: Bob Weinand <bob.weinand@datadoghq.com>
    
    * Update zend_abstract_interface/functions/php5/functions.c
    
    Co-authored-by: Sammy Kaye Powers <sammyk@datadoghq.com>
    
    Co-authored-by: Sammy Kaye Powers <sammyk@datadoghq.com>

--- ext/php7/ddtrace.c
+++ ext/php7/ddtrace.c
@@ -500,7 +500,6 @@ static PHP_RINIT_FUNCTION(signalfx_tracing) {
     }
 
     ddtrace_internal_handlers_rinit();
-    ddtrace_engine_hooks_rinit();
     ddtrace_bgs_log_rinit(PG(error_log));
     ddtrace_dispatch_init();
     DDTRACE_G(disable_in_current_request) = 0;
@@ -556,7 +555,6 @@ static PHP_RSHUTDOWN_FUNCTION(signalfx_tracing) {
     zend_array_destroy(DDTRACE_G(additional_global_tags));
     ZVAL_NULL(&DDTRACE_G(additional_trace_meta));
 
-    ddtrace_engine_hooks_rshutdown();
     ddtrace_internal_handlers_rshutdown();
     ddtrace_dogstatsd_client_rshutdown();
 
