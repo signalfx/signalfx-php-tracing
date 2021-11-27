9f2fa9ec2a75694243ccf15521331e6c7e934f15
Siim Kallas <siimkallas@gmail.com>
2021-11-27 23:30:44 +0200

    populate drupal controller

--- src/DDTrace/Integrations/Drupal/DrupalIntegration.php
+++ src/DDTrace/Integrations/Drupal/DrupalIntegration.php
@@ -285,7 +285,7 @@ class DrupalIntegration extends Integration
         \DDTrace\trace_method(
             'Drupal\Component\EventDispatcher\ContainerAwareEventDispatcher',
             'dispatch',
-            function (SpanData $span, $args) {
+            function (SpanData $span, $args, $ret) {
                 if (!empty($args[0]) && is_string($args[0])) {
                     $name = $args[0];
                 } elseif (!empty($args[1]) && is_string($args[1])) {
@@ -303,6 +303,12 @@ class DrupalIntegration extends Integration
                 }
 
                 $span->name = 'drupal.event.' . $name;
+                if (method_exists($ret, 'getRequest')) {
+                   $controller = $ret->getRequest()->get('_controller');
+                   if ($controller) {
+                       $span->meta['drupal.controller'] = $controller;
+                   }
+                }
             }
         );
     }
