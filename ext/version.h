3304ff6fc69746365f5cb0e09f787d79534f1655
Siim Kallas <siimkallas@gmail.com>
2021-11-29 20:04:00 +0200

    update version

--- ext/version.h
+++ ext/version.h
@@ -1,4 +1,4 @@
 #ifndef PHP_DDTRACE_VERSION
 // Must begin with a number for Debian packaging requirements
-#define PHP_DDTRACE_VERSION "0.58.0-beta-sfx5"
+#define PHP_DDTRACE_VERSION "0.58.0-beta-sfx6"
 #endif
