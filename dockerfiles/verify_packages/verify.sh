DataDog/dd-trace-php

PHP installer (#1335)
* draft of PHP installation script

* start validating options in php installer

* setup integration testing for the script

* rename installation script file

* installer: add functions to build the known commands names matrix

* installer: add function to search for relevant php binaries + tests

* installer: finalize function to read relevant INI values

* installer: add progress indication to download

* installer: add function to pick interactively the php binaries for installation

* installer: installation until extension copy

* installer: start adding ini files template

* installer: add comments to explain INI settings

* installer: remove indentation from heredoc in INI template

* installer: do not run if ddtrace installed + rename tracer-specific options

* installer: remove check to exit if ddtrace module is detected

* installer: move instead of copying the .so file to avoid a segfault

* installer: replace the link to the right dd_wrap_autoloader.php

* installer: use always the same temporarily extension name before renaming

* installer: fix INI replacement sed and indentation for phpcs

* installer: add the possibility to install to all binaries

* installer: add package verification to CI

* installer: fix linting

* installer: use small resources in CI for package verification and remove obsolete unused executor

* installer: do not use cache while installing libexecinfo

* installer: make installer compatible with php 7.2-

* installer: grab API version from PHP API ini info

* installer: fix proper installation/verification of new installer on debian

* installer: support apache2 conf.d in debian based envs

* installer: rename to dd-library-php-setup.php

* installer: add checks for libcurl and fallbacks while downloading the installable archive

* installer: support uninstall

* installer: test and fix upgrade process

* installer: test and fix upgrade proc

dockerfiles/verify_packages/verify.sh
@@ -33,11 +33,11 @@ if [ -z "$DD_TRACE_PHP_BIN" ]; then
     DD_TRACE_PHP_BIN=$(command -v php5 || true)
 fi
 
-echo "PHP version: $(php -v)"
+echo "PHP version: $(${DD_TRACE_PHP_BIN} -v)"
 
 # Script output
 CLI_OUTPUT=$(DD_TRACE_CLI_ENABLED=true ${DD_TRACE_PHP_BIN} /var/www/html/index.php)
-if [ ! "${CLI_OUTPUT}" == "hi" ]; then
+if [ "${CLI_OUTPUT}" != "hi" ]; then
     echo "Error: expected request output is 'hi'. Actual:\n${APACHE_OUTPUT}"
     exit 1
 else
@@ -48,7 +48,7 @@ fi
 sleep 1
 CLI_TRACES=$(curl -s -L request-replayer/replay)
 # sh compatible way to do string contains
-if [ "${CLI_TRACES#*trace_id}" == "${CLI_TRACES}" ]; then
+if [ "${CLI_TRACES#*trace_id}" = "${CLI_TRACES}" ]; then
     echo "Error: traces have not been sent correctly. From request replayer:\n${CLI_TRACES}"
     exit 1
 else
@@ -67,7 +67,7 @@ curl -s -L request-replayer/clear-dumped-data
 
 # Request output
 NGINX_OUTPUT=$(curl -s -L localhost:8080)
-if [ ! "${NGINX_OUTPUT}" == "hi" ]; then
+if [ "${NGINX_OUTPUT}" != "hi" ]; then
     echo "Error: expected request output is 'hi'. Actual:\n${NGINX_OUTPUT}"
     exit 1
 else
@@ -78,7 +78,7 @@ fi
 sleep 2
 NGINX_TRACES=$(curl -s -L request-replayer/replay)
 # sh compatible way to do string contains
-if [ "${NGINX_TRACES#*trace_id}" == "${NGINX_TRACES}" ]; then
+if [ "${NGINX_TRACES#*trace_id}" = "${NGINX_TRACES}" ]; then
     echo "Error: traces have not been sent correctly. From request replayer:\n${NGINX_TRACES}"
     exit 1
 else
@@ -98,7 +98,7 @@ if [ "${VERIFY_APACHE}" != "no" ]; then
 
     # Request output
     APACHE_OUTPUT=$(curl -s -L localhost/index.php)
-    if [ ! "${APACHE_OUTPUT}" == "hi" ]; then
+    if [ "${APACHE_OUTPUT}" != "hi" ]; then
         echo "Error: expected request output is 'hi'. Actual:\n${APACHE_OUTPUT}"
         exit 1
     else
@@ -109,7 +109,7 @@ if [ "${VERIFY_APACHE}" != "no" ]; then
     sleep 2
     APACHE_TRACES=$(curl -s -L request-replayer/replay)
     # sh compatible way to do string contains
-    if [ "${APACHE_TRACES#*trace_id}" == "${APACHE_TRACES}" ]; then
+    if [ "${APACHE_TRACES#*trace_id}" = "${APACHE_TRACES}" ]; then
         echo "Error: traces have not been sent correctly. From request replayer:\n${APACHE_TRACES}"
         exit 1
     else

dockerfiles/verify_packages/verify_rpm_centos6.sh
@@ -10,10 +10,19 @@ fi
 for phpVer in $(ls ${PHP_INSTALL_DIR}); do
     echo "Installing ddtrace on PHP ${phpVer}..."
     switch-php $phpVer
-    rpm -Uvh /build_src/build/packages/*.rpm
-    php --ri=ddtrace
 
-    # Uninstall the tracer
-    rpm -e datadog-php-tracer
-    rm -f /opt/datadog-php/etc/ddtrace.ini
+    # Installing dd-trace-php
+    INSTALL_TYPE="${INSTALL_TYPE:-php_installer}"
+    if [ "$INSTALL_TYPE" = "native_package" ]; then
+        echo "Installing dd-trace-php using the OS-specific package installer"
+        rpm -Uvh /build_src/build/packages/*.rpm
+        php --ri=ddtrace
+
+        # Uninstall the tracer
+        rpm -e datadog-php-tracer
+        rm -f /opt/datadog-php/etc/ddtrace.ini
+    else
+        echo "Installing dd-trace-php using the new PHP installer"
+        php /build_src/dd-library-php-setup.php --tracer-file /build_src/build/packages/*.tar.gz --php-bin all
+    fi
 done

signalfx/signalfx-php-tracing

dockerfiles/verify_packages/verify.sh
@@ -33,12 +33,12 @@ if [ -z "$DD_TRACE_PHP_BIN" ]; then
     DD_TRACE_PHP_BIN=$(command -v php5 || true)
 fi
 
-echo "PHP version: $(php -v)"
+echo "PHP version: $(${DD_TRACE_PHP_BIN} -v)"
 
 # Script output
 CLI_OUTPUT=$(SIGNALFX_TRACE_CLI_ENABLED=true SIGNALFX_TRACE_DEBUG=true ${DD_TRACE_PHP_BIN} /var/www/html/index.php)
-if [ ! "${CLI_OUTPUT}" == "hi" ]; then
-    echo "Error: expected request output is 'hi'. Actual:\n${CLI_OUTPUT}"
+if [ "${CLI_OUTPUT}" != "hi" ]; then
+    echo "Error: expected request output is 'hi'. Actual:\n${APACHE_OUTPUT}"
     exit 1
 else
     echo "Request output is correct"
@@ -50,7 +50,7 @@ CLI_TRACES=$(curl -s -L request-replayer/replay)
 echo "CLI traces"
 echo "${CLI_TRACES}"
 # sh compatible way to do string contains
-if [ "${CLI_TRACES#*traceId}" == "${CLI_TRACES}" ]; then
+if [ "${CLI_TRACES#*traceId}" = "${CLI_TRACES}" ]; then
     echo "Error: traces have not been sent correctly. From request replayer:\n${CLI_TRACES}"
     exit 1
 else
@@ -69,7 +69,7 @@ curl -s -L request-replayer/clear-dumped-data
 
 # Request output
 NGINX_OUTPUT=$(curl -s -L localhost:8080)
-if [ ! "${NGINX_OUTPUT}" == "hi" ]; then
+if [ "${NGINX_OUTPUT}" != "hi" ]; then
     echo "Error: expected request output is 'hi'. Actual:\n${NGINX_OUTPUT}"
     exit 1
 else
@@ -83,7 +83,7 @@ NGINX_TRACES=$(curl -s -L request-replayer/replay)
 echo "nginx traces"
 echo "${NGINX_TRACES}"
 # sh compatible way to do string contains
-if [ "${NGINX_TRACES#*traceId}" == "${NGINX_TRACES}" ]; then
+if [ "${NGINX_TRACES#*traceId}" = "${NGINX_TRACES}" ]; then
     echo "Error: traces have not been sent correctly. From request replayer:\n${NGINX_TRACES}"
     exit 1
 else
@@ -103,7 +103,7 @@ if [ "${VERIFY_APACHE}" != "no" ]; then
 
     # Request output
     APACHE_OUTPUT=$(curl -s -L localhost/index.php)
-    if [ ! "${APACHE_OUTPUT}" == "hi" ]; then
+    if [ "${APACHE_OUTPUT}" != "hi" ]; then
         echo "Error: expected request output is 'hi'. Actual:\n${APACHE_OUTPUT}"
         exit 1
     else
@@ -116,7 +116,7 @@ if [ "${VERIFY_APACHE}" != "no" ]; then
     echo "apache traces"
     echo "${APACHE_TRACES}"
     # sh compatible way to do string contains
-    if [ "${APACHE_TRACES#*traceId}" == "${APACHE_TRACES}" ]; then
+    if [ "${APACHE_TRACES#*traceId}" = "${APACHE_TRACES}" ]; then
         echo "Error: traces have not been sent correctly. From request replayer:\n${APACHE_TRACES}"
         exit 1
     else

dockerfiles/verify_packages/verify_rpm_centos6.sh
@@ -10,10 +10,19 @@ fi
 for phpVer in $(ls ${PHP_INSTALL_DIR}); do
     echo "Installing signalfx-php-tracing on PHP ${phpVer}..."
     switch-php $phpVer
-    rpm -Uvh /build_src/build/packages/*.rpm
-    php --ri=signalfx_tracing
 
-    # Uninstall the tracer
-    rpm -e signalfx-tracing
-    rm -f /opt/signalfx-php-tracing/etc/ddtrace.ini
+    # Installing dd-trace-php
+    INSTALL_TYPE="${INSTALL_TYPE:-php_installer}"
+    if [ "$INSTALL_TYPE" = "native_package" ]; then
+        echo "Installing signalfx-php-tracing using the OS-specific package installer"
+        rpm -Uvh /build_src/build/packages/*.rpm
+        php --ri=signalfx_tracing
+
+        # Uninstall the tracer
+        rpm -e signalfx_tracing
+        rm -f /opt/signalfx-php-tracing/etc/ddtrace.ini
+    else
+        echo "Installing signalfx-php-tracing using the new PHP installer"
+        php /build_src/dd-library-php-setup.php --tracer-file /build_src/build/packages/*.tar.gz --php-bin all
+    fi
 done